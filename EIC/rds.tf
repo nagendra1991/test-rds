d53491a9f4085801ef317b24e6abefbaac7e4cfe
nagendra1991 <46521601+nagendra1991@users.noreply.github.com>
2020-06-15 18:37:40 +0530

    c7

--- EIC/rds.tf
+++ EIC/rds.tf
@@ -2,11 +2,9 @@ module "db" {
   source        = "../modules"
   subnetgroup   = "Subnet_Id"
   engine        = "databasetype"
-  securitygroup = "sgids" 
   engineversion = "dbVersion"
   storage       = "storagesize"
   name          = "dbname"
   instanceclass = "dbtype"
   username      = "dbusername"
   password      = "dbuserpwd" 
-}
