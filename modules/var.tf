23b93ff66020f4c78f79b447a5bb2fac73ad887b
nagendra1991 <46521601+nagendra1991@users.noreply.github.com>
2020-06-15 18:10:03 +0530

    commit 3

--- modules/var.tf
+++ modules/var.tf
@@ -1,26 +1,19 @@
 variable "storage" {
-    default = "storagesize"
 }
 variable "engine" {
-  default = "databasetype"
 }
 variable "engineversion" {
-  default = "dbVersion"
 }
 variable "identifier" {
   default = "identifier"
 }
 variable "name" {
-  default = "dbname"
 }
 variable "username" {
-  default = "root"
 }
 variable "password" {
-  default = "admin1234"
 }
 variable "subnetgroup" {
-  default = "subnet"
 }
 variable "parametergroup" {
   default = "parametergroup"
@@ -29,13 +22,8 @@ variable "azone" {
   default = "ap-southeast-1"
 }
 variable "securitygroup" {
-  default = "sggroup"
-}
-variable "nametag" {
-  default = "name"
 }
 variable "instanceclass" {
-  default = ""
 }
 variable "storagetype" {
   default = "gp2"
