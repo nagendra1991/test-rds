24186920630c2971d9a286b26bf61dc3d4d6ea7e
nagendra1991 <46521601+nagendra1991@users.noreply.github.com>
2020-06-15 18:31:37 +0530

    c6

--- modules/main.tf
+++ modules/main.tf
@@ -1,18 +1,18 @@
 resource "aws_db_instance" "db" {
-  allocated_storage       = "${var.storage}"
-  storage_type            = "${var.storagetype}"
-  engine                  = "${var.engine}"
-  engine_version          = "${var.engineversion}"
-  instance_class          = "${var.instanceclass}" # use micro if you want to use the free tier
-  identifier              = "${var.identifier}"
-  name                    = "${var.name}"
-  username                = "${var.username}" # username
-  password                = "${var.password}" # password
-  db_subnet_group_name    = "${var.subnetgroup}"
-  parameter_group_name    = "${var.parametergroup}"
-  availability_zone       = "${var.azone}" # prefered AZ 
+  allocated_storage       = var.storage
+  storage_type            = var.storagetype
+  engine                  = var.engine
+  engine_version          = var.engineversion
+  instance_class          = var.instanceclass # use micro if you want to use the free tier
+  identifier              = var.identifier
+  name                    = var.name
+  username                = var.username # username
+  password                = var.password # password
+  db_subnet_group_name    = var.subnetgroup
+  parameter_group_name    = var.parametergroup
+  availability_zone       = var.azone # prefered AZ 
   multi_az                = "false"        # set to true to have high availability: 2 instances synchronized with each other
-  vpc_security_group_ids  = ["${var.securitygroup}"]
+  vpc_security_group_ids  = [var.securitygroup]
   backup_retention_period = 30   # how long we are going to keep your backups 
   skip_final_snapshot     = true # skip final snapshot when doing terraform destroy
 }
