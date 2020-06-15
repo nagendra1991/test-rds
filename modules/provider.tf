01ee9e84e4d59f06413800cff3c8810255c94760
nagendra1991 <46521601+nagendra1991@users.noreply.github.com>
2020-06-15 19:41:21 +0530

    provider v1

--- modules/provider.tf
+++ modules/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-   access_key = "AKIAJ4PVGWYWZXXGXZIQ"
-  secret_key = "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"
-  region     = "ap-south-1"
+   access_key = "AKIAIBPUEPDEHRJY2OGA"
+  secret_key = "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"
+  region     = "us-east-1"
   version    = "~>2.65"
 }
